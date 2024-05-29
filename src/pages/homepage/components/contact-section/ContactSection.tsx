72aa0e45205f4405be6fede504ce86cfe4f6ccd6
Charles Owusu <charlesowusu274@gmail.com>
2024-05-29 10:30:15 +0100

    .

--- src/pages/homepage/components/contact-section/ContactSection.tsx
+++ src/pages/homepage/components/contact-section/ContactSection.tsx
@@ -55,8 +55,9 @@ const ContactSection = () => {
               {/* frameborder={0} */}
 
               <iframe
-                src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d12097.433213460943!2d-74.0062269!3d40.7101282!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xb89d1fe6bc499443!2sDowntown+Conference+Center!5e0!3m2!1smk!2sbg!4v1539943755621"
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3978.5485925302075!2d-0.20311638529444034!3d5.603716995946975!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xfdf9adbf53bc1af%3A0x15ff084d8f1c157!2sAccra%2C%20Ghana!5e0!3m2!1sen!2sus!4v1622489380335!5m2!1sen!2sus"
                 style={{ border: "0", width: "100%", height: "290px" }}
+                loading="lazy"
               ></iframe>
             </div>
           </div>
@@ -68,10 +69,10 @@ const ContactSection = () => {
               </div>
               <div className="card-body">
                 <form
-                  action="forms/contact.php"
+                  action="#"
                   method="post"
                   role="form"
-                  className="php-email-form "
+                  className="contact-form"
                 >
                   {/* Name */}
                   <div className="row">
